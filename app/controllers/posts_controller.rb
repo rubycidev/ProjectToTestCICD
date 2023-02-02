383f77c6717ca50fa84ea8a34f5f7d87dc0e74f9
Junior Fernandes <jrfernandesti@gmail.com>
2023-02-02 16:20:52 -0300

    set buddy ci

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
 
-  # GET /posts or /posts.json
+  # GET /posts or /posts.json ####
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1 or /posts/1.json
+  # GET /posts/1 or /posts/1.json ####
   def show
   end
 
