0259f3061ae157a87917b9aa9d250e0c610ba11a
Junior Fernandes <jrfernandesti@gmail.com>
2023-02-23 09:16:43 -0300

    test: added complex specs

--- spec/models/post_test_spec.rb
+++ spec/models/post_test_spec.rb
@@ -1,6 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  def fatorial (n)
+    if (n > 1)
+       return n * fatorial(n - 1)
+    else
+       return 1
+    end
+  end
+
+  describe 'to test ci' do
+    it 'testing' do
+      result = fatorial(9).times { |c| puts c * 1350 }
+
+      expect(result).to eq(362880)
+    end
+    it 'testing' do
+      result = fatorial(9).times { |c| puts c * 1350 }
+
+      expect(result).to eq(362880)
+    end
+    it 'testing' do
+      result = fatorial(9).times { |c| puts c * 1350 }
+
+      expect(result).to eq(362880)
+    end
+    it 'testing' do
+      result = fatorial(9).times { |c| puts c * 1350 }
+
+      expect(result).to eq(362880)
+    end
+    it 'testing' do
+      result = fatorial(9).times { |c| puts c * 1350 }
+
+      expect(result).to eq(362880)
+    end
+  end
   describe 'attributes' do
     it { is_expected.to respond_to :name }
     it { is_expected.to respond_to :title }
